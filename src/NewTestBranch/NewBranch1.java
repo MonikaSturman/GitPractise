c940f499b315570887d78a1b05ff03638663580f
Monika <monikasturman@gmail.com>
2023-07-04 23:34:40 +0100

    New test branch uploaded

---
+++ src/NewTestBranch/NewBranch1.java
@@ -0,0 +1,9 @@
+package NewTestBranch;
+
+public class NewBranch1 {
+    public static void main(String[] args) {
+
+        System.out.println("This is the second Branch");
+        // we are in NewBranch2
+    }
+}
