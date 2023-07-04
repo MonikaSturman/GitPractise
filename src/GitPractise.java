ac129407c1762c24a76e96cb62e1afff016806f9
Monika <monikasturman@gmail.com>
2023-07-04 22:29:10 +0100

    GitPractise upload

---
+++ src/GitPractise.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GitPractise {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Sarah");
+        names.add("Peter");
+        names.add("Maria");
+        names.add("Meral");
+        names.add("Hakan");
+        names.add("Monika");
+        names.add("Ahmet");
+        names.add("Talha");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean foundName = false;
+        System.out.println("Welcome to BMW annual event!!");
+
+        while (!foundName) {
+
+            System.out.println("Please enter your name to entry: ");
+            String searchName = scanner.next();
+
+
+
+            if (names.contains(searchName)) {
+                System.out.println(searchName + " is in the guest list :) WELCOME!");
+                break;
+            } else {
+                System.out.println(searchName + " is not in the guest list :( Try again!");
+            }
+        }
+
+    }
+
+}
